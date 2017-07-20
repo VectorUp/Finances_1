e64760cfa3637eebab00a9d9a0bbdde4bc4013c2
Kostya <marker99@rambler.ru>
2017-07-20 15:17:27 +0300

    Edit_text_and_New_activity

---
+++ app/src/main/java/com/example/shpp_admin/myapplication/LeftActivity.java
@@ -0,0 +1,13 @@
+package com.example.shpp_admin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_left);
+    }
+}
--- app/src/main/java/com/example/shpp_admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shpp_admin/myapplication/MainActivity.java
@@ -3,19 +3,22 @@ package com.example.shpp_admin.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView text =(TextView) findViewById( R.id.textView);
-        text.setText("kjgjghjg");
-        View buttonRight = findViewById(R.id.buttonRight);
+
+        ImageButton buttonRight = (ImageButton) findViewById(R.id.buttonRight);
         buttonRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +26,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //jmyujmum
+        ImageButton buttonLeft = (ImageButton) findViewById(R.id.buttonLeft);
+        buttonLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LeftActivity.class);
+                startActivity(intent);
+            }
+        });
+        // Ввод текста с выводом при нажатии Enter
+        final EditText editText = (EditText)findViewById(R.id.EditTextNumber);
+        textView = (TextView)findViewById(R.id.TextViewSumm);
+        editText.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    String moneyTotal = editText.getText().toString();
+
+                    if (editText.getText().toString().equals(""))
+                        textView.setText("empty");
+                    else
+                        textView.setText(moneyTotal);
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
-//    gitignore
-}
+        //jmyujmum
+        // gitignore
+    }
+
+
