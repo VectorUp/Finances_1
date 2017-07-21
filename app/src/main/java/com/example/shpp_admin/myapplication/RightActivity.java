043422ad98230bbd51679b77cc11de73700b1dec
Kostya <marker99@rambler.ru>
2017-07-21 11:27:11 +0300

    right_activity_new

--- app/src/main/java/com/example/shpp_admin/myapplication/RightActivity.java
+++ app/src/main/java/com/example/shpp_admin/myapplication/RightActivity.java
@@ -1,7 +1,12 @@
 package com.example.shpp_admin.myapplication;
 
+import android.content.Intent;
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RightActivity extends AppCompatActivity{
 
@@ -11,6 +16,30 @@ public class RightActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_right);
     }
+
+
+    public void onCommitClick(View view) {
+        EditText total = (EditText) findViewById(R.id.totalNumber);
+        EditText days = (EditText) findViewById(R.id.totalDays);
+        int totalInt = Integer.parseInt(total.getText().toString());
+        int daysInt = Integer.parseInt(days.getText().toString());
+        int perDay;
+
+        if (daysInt > 0)
+            perDay = (totalInt/daysInt);
+        else
+            perDay = 0;
+
+        Intent intent = new Intent(RightActivity.this, MainActivity.class);
+        intent.putExtra("per_day", perDay);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(perDay));
+
+        //startActivity(intent);
+    }
+
+
 //    activity lifecycle
 //    extends/implements
 //    recycler view
