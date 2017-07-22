6240e553bf22435ddfb1fedf60a9bae375babf98
Kostya <marker99@rambler.ru>
2017-07-22 14:46:29 +0300

    dialog_new_new

--- app/src/main/java/com/example/shpp_admin/myapplication/DialogActivity.java
+++ app/src/main/java/com/example/shpp_admin/myapplication/DialogActivity.java
@@ -28,6 +28,11 @@ public class DialogActivity extends DialogFragment implements OnClickListener {
     }
 
     public void onClick (View view) {
+        switch (view.getId()) {
+            case R.id.buttonCreate:
+
+        }
+
         dismiss();
     }
 
