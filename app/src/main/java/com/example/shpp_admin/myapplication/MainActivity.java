2869c5fe7e4503f7673ef7fd56416ffef510a2a8
Kostya <marker99@rambler.ru>
2017-07-24 14:32:33 +0300

    aaaaa

--- app/src/main/java/com/example/shpp_admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shpp_admin/myapplication/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        textView = (TextView) findViewById(R.id.EditTextNumber);
+        textView = (TextView) findViewById(R.id.TextNumber);
         textView.setText(getIntent().getStringExtra("class"));
     }
 }
